8e94025c3dc3d81eb6262e6f63ab001b61a94a87
jbwexler <jbwexler@tutanota.com>
2022-04-20 00:42:13 -0500

    unlock freesurfer scripts

--- sourcedata/freesurfer/sub-16/scripts/recon-all.cmd
+++ sourcedata/freesurfer/sub-16/scripts/recon-all.cmd
@@ -1,1 +1,1 @@
-../../../../.git/annex/objects/jm/3m/MD5E-s5325--aa0835a813049b0b4afadc68c7966548.cmd/MD5E-s5325--aa0835a813049b0b4afadc68c7966548.cmd
+/annex/objects/MD5E-s5325--aa0835a813049b0b4afadc68c7966548.cmd
